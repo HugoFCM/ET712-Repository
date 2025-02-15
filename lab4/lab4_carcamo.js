3c2b15b54dfda57cf3d32c54e7c8f677100a47e8
HugoFCM <fernandocarcamo825@gmail.com>
2025-02-14 21:04:01 -0500

    Add files via upload

--- lab4/lab4_carcamo.js
+++ lab4/lab4_carcamo.js
@@ -53,10 +53,73 @@ console.log(`The double of number ${number} is ${dbnum}`)
 console.log("\n--------- Example 6: function that returns value ---------")
 // function that returns 'true' if a temperature is greater than 75. Otherwise, it returns a 'false' if the temperature is less than or equal to 75
 function checktemp(temperature){
-    if(temperature>75){
-        return true
+    if(temperature > 75){
+        return true;
+    } else {
+        return false;
     }
-    else{
-        return false
-    }
+} 
+
+console.log("\n------ Example 7: Built-in Functions ------")
+// built-in function in JS is called as a global variable
+// Math function
+let num_pi = Math.PI
+console.log(`JS PI value = ${num_pi}`)
+// round() method returns the nearest integer
+console.log(`rounded off PI = ${Math.round(num_pi)}`)
+// ceil() method returns the round up of a number
+console.log(`ceil method applied to PI = ${Math.ceil(num_pi)}`)
+// pow() method returns the value of base to the power of the exponent
+console.log(`2^3 = ${Math.pow(2,3)}`)
+// square root method --> sqrt()
+console.log(`square root of 9 = ${Math.sqrt(20)}`)
+// random number generator random() method returns a random number between 0 and 1
+console.log(`Random number = ${Math.random()}`)
+// random number between 1 and 9
+let n = 1 + Math.round(Math.random()*8)
+console.log(`Number between 1 and 9 = ${n}`)
+
+console.log("\n------ Example 8: Random number ------")
+// function to randomly generate a number between -5 and 5
+function randnumber(){
+    let p = -5 + Math.round(Math.random()*10)
+    return p;
 }
+
+console.log("\n------ Example 9: Random number in a list (array) ------")
+// function to randomly pick a color from a list of colors
+let colors = ["red","green","blue","magenta","gray"]
+function randomcolor(picked_index){
+    let list_size = colors.length  // find the length of the list
+    let last_index = list_size - 1  // find the last index of the list
+    let random_index = Math.round((Math.random()*last_index))   // randomly pick a number between 0 and the last index
+    return colors[random_index] // return the random picked color
+}
+
+console.log(`Color picked = ${randomcolor()}`)
+
+console.log("\n------ EXERCISE ------")
+
+function checkName() {
+    let name;
+    
+    while (true) {
+        name = prompt("Enter your name:");
+
+        if (name === null || name.trim() === "") {  
+            
+            name = prompt("You forgot to enter a name. Enter a name again:");
+        } else if (!isNaN(name)) {  
+            
+            name = prompt(`${name} is invalid! Enter a name again:`);
+        } else {
+
+            break;
+        }
+    }
+
+    console.log(`Welcome ${name.toUpperCase()} to the class!`);
+}
+
+checkName();
+
