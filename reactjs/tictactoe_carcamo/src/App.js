dd71b64961dfc721b9db05c65f6ec28fadf91558
Hugo Carcamo <HUGO.CARCAMO10@student.qcc.cuny.edu>
2025-04-20 18:18:55 -0400

    app

--- reactjs/tictactoe_carcamo/src/App.js
+++ reactjs/tictactoe_carcamo/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='title'>Tic Tac Toe by Hugo Carcamo</h1>
+      <button className="square">&#9932;</button>
     </div>
   );
 }
