00c664169e1b98007b1c0b49868f703021f7e288
HugoFCM <fernandocarcamo825@gmail.com>
2025-05-14 15:34:54 +0000

    p2t2

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
 import rbnyimg from './images/rbny.png';
+import userlogo from './images/user.png';
+import scart from './images/cart.png';
+import rbb from './images/lgrb.jpg';
 
 function App() {
   return (
-  <>
-  <header className='header'>
-    <section className='logo'>
-       <img src={rbnyimg} style={{width:"50px", display:"block", margin:"auto"}}/>
-    </section>
-    <h3 className='message'>RED RUNS DEEP.</h3>
+    <>
+<header className='header'>
+  <section className='logo'>
+    <img src={rbnyimg} alt="RBNY Logo" />
+  </section>
 
-  </header>
-  </>
+  <h3 className='message'>RED RUNS DEEP.</h3>
+
+  <div className='spacer'></div>
+
+  <div className='icon-section'>
+    <img src={scart} alt="Cart" className='icon' />
+    <div className='divider'></div>
+    <img src={userlogo} alt="User" className='icon' />
+  </div>
+</header>
+
+<div className="hero"></div>  
+
+    </>
   );
 }
 
